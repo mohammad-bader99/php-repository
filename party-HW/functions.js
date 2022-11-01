413ab478cb478eaeb8d9063fd1b9b96294372d13
mohammad bader <mohammadbader246@gmail.com>
2022-11-01 23:03:33 +0200

    Create functions.js

---
+++ party-HW/functions.js
@@ -0,0 +1,53 @@
+// Function
+
+let studentAge = 22;
+if (studentAge >= 22) {
+  acceptStudent();
+} else {
+  rejectStudent();
+}
+
+// User-defined functions
+function acceptStudent() {
+  // send welcoming email
+  // add to the accepted sheet
+}
+
+function rejectStudent() {
+  // send regret email.
+  // add to the refusal sheet
+}
+
+let arr = [1, 2, 3];
+// arr = [1, 2, 3, 4];
+
+arr.push(4);
+// console.log(arr);
+
+let Obj = {
+  name: "Khalid",
+  age: 20,
+  speek: function () {
+    console.log("This is Khalid");
+  },
+};
+
+function printHi() {
+  console.log("hi");
+}
+
+function getGreeting() {
+  return "hi";
+}
+
+let a = 1;
+let b = 3;
+// console.log(a + b);
+
+function addTo(a, b) {
+  return a + b;
+}
+// console.log(addTo(1, 3));
+// console.log(addTo(4, 5));
+
+// Array methods
