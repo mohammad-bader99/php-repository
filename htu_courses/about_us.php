a4faf8be69e0eb86085caca2f7fdb5b6a685a7d0
mohammad bader <mohammadbader246@gmail.com>
2022-11-24 07:15:54 +0200

    Create about_us.php

---
+++ htu_courses/about_us.php
@@ -0,0 +1,21 @@
+<?php
+require './header.php'; ?>
+<script>document.getElementById("slogan").style.display="none";</script>
+
+<main style="width: 100%;">
+    <div class="d-flex justify-content-around text-center my-5">
+        <div style="width: 40%">
+         <img src="./assets/images/HTU Logo200.png" class="border border-2 my-5 rounded-circle bg-success p-2 text-white bg-opacity-75" alt="htu logo">
+        </div>
+        <div style="width: 40%">
+            <h1>HTU</h1>
+            <hr class="border border-primary border-3 opacity-75">
+            <p class="my-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptates illum voluptate ab sed ipsam ad harum quisquam dignissimos sint aspernatur eligendi, repellendus ullam facilis, repudiandae vitae nesciunt esse exercitationem?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse illum accusamus, quibusdam neque iste harum tenetur vel excepturi sequi veniam ut commodi minima delectus quis optio iusto quam et rem.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis autem quos vel rem doloribus impedit repellendus ex itaque voluptates. Temporibus quibusdam perspiciatis voluptatem laudantium facilis dolorum nesciunt porro aut!
+            </p>
+        </div>
+    </div>
+</main>
+
+<?php require './footer.php'; ?>
