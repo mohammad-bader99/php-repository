eb6657d4bad5939cff89c756ded2251f43418bc3
mohammad bader <mohammadbader246@gmail.com>
2022-12-02 03:31:24 +0200

    Update function.php

--- seat reservation/function.php
+++ seat reservation/function.php
@@ -7,8 +7,28 @@ class user_db
 
    function __construct()
    {
-    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "htu";
+
+    $this->conn = mysqli_connect($servername, $username, $password, $database);
    }
+
+   function create_user($first_name,$last_name,$email,$password)
+    {
+
+        if( isset($first_name) && isset($last_name) && isset($email) && isset($password))
+    {
+        $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
+        mysqli_query($this->conn, $sql);
+        
+        header('Location: ./index.php');
+    }
+    else{
+        header('Location: ./user.php');
+    }}
+
 
 
 }
