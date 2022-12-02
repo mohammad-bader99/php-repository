cf0f3a65bb885e78220a934713361f254ca0622f
mohammad bader <mohammadbader246@gmail.com>
2022-12-02 03:31:18 +0200

    Update index.php

--- seat reservation/index.php
+++ seat reservation/index.php
@@ -3,8 +3,12 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 
-<div class="container" action="" style="margin-top:15%;">
-    
+<div class="container" action="" style="margin-top:30px;">
+
+<div class="container m-auto">
+    <p>admin@test.com<br>password:1234<br><br>test@test.com<br>password:1234</p>
+</div>
+
    <form class="m-auto w-50 border border-3 p-1" action="" method="GET">
 
    <div class="mb-3 row">
