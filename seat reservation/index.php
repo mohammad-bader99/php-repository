f153fb1bf8ab8881becea34f860c97f212a4490d
mohammad bader <mohammadbader246@gmail.com>
2022-12-02 03:15:12 +0200

    Create index.php

---
+++ seat reservation/index.php
@@ -0,0 +1,31 @@
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+<div class="container" action="" style="margin-top:15%;">
+    
+   <form class="m-auto w-50 border border-3 p-1" action="" method="GET">
+
+   <div class="mb-3 row">
+    <label for="inputEmail" class="col-sm-2 col-form-label">Email:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="email" name="email" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password:</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+  </div>
+    <div class="text-center">
+        <input type="submit" class="btn btn-primary w-50 " value="Login">
+    </div>
+    <div class="text-end">
+        <a class="btn btn-link" href="./registration.php">Register Now!</a>
+    </div>
+
+</form>
+
+</div>
